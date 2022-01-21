3685932bed20b29f39730b41cd145963834e6923
James Prentice <jamesprenticez@gmail.com>
2022-01-21 21:13:03 +1300

    JP Logo

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,13 +1,17 @@
 function Main() {
   return (
-    <div className="w-full flex flex-wrap justify-center">
-      <div className="w-full h-screen bg-black p-4 font-black text-white">
-        <p>one</p>
+    <div className='w-full flex flex-wrap justify-center'>
+      <div className='w-full h-screen bg-black p-4'>
+        {/* <p className='text-7xl font-black absolute stretch text-blue-500'>J</p> */}
+        <p className='text-7xl font-black absolute select-none text-green-600'>J</p>
+        <p className='text-7xl font-black absolute select-none text-white ml-[0.3rem]'>J</p>
+        <p className='text-7xl font-black absolute select-none text-green-600  ml-[2.8rem] '>P</p>
+        <p className='text-7xl font-black absolute select-none text-white ml-[3.1rem]'>P</p>
       </div>
-      <div className="w-full h-screen bg-green-500 p-4">
+      <div className='w-full h-screen bg-green-500 p-4'>
         two
       </div>
-      <div className="w-full h-screen bg-blue-500 p-4">
+      <div className='w-full h-screen bg-blue-500 p-4'>
         three
       </div>
     </div>
