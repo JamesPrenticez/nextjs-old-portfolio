b7bc8f7f33dd69b769dcb93f3f710b1c112386dd
James Prentice <jamesprenticez@gmail.com>
2022-01-23 23:25:06 +1300

    Clean up

--- components/List.jsx
+++ components/List.jsx
@@ -1,41 +1,16 @@
 import { projects } from '../data';
-import React, { useEffect, useRef, useState } from 'react';
-
+import React, { useRef, useState } from 'react';
 
 function List() {
-
   const cardRef = useRef([])
   //const [data, setData] = useState(['1', '2', '3', '4', '5']
   const [data, setData] = useState(projects) //arry of objects
-
-  const randomColor = () => {
-    return Math.floor(Math.random()*16777215).toString(16);
-  }
-
-  //Dynamically append a useRef arry
-  useEffect(() => {
-    cardRef.current = cardRef.current.slice(0, data.length);
-  }, [cardRef, data]);
-
-  const add = () => {
-    let increment = data.length
-    let color = randomColor()
-    setData([...data, {id: increment, position: increment, title: `${color}`, color: `#${color}`}])
-    //console.log(cardRef.current)
-  }
-
-  const subtract = () => {
-    let a = data
-    let newData = a.filter(p => p.position != a[a.length -1].position)
-    setData(newData)
-  }
 
   const shuffle = () => {
     let list = data
     let i = list.length -1, randomI, currentPos, randomPos
     //While there remains elements to shuffle... works top back to 0 index
     while(i >= 1){
-
       randomI = Math.ceil(Math.random() * i--)
 
       //Find a pair to swap
@@ -52,22 +27,19 @@ function List() {
   }
 
   const sort = () => {
-    console.log("hi")
+    console.log('sort')
   }
 
   return (
   <>
     <button type="button" className='px-8 py-4 bg-purple-500' onClick={() => shuffle()}>Shuffle</button>
     <button type="button" className='px-7 py-4 bg-yellow-500' onClick={() => sort()}>Sort</button>
-    <button type="button" className='px-7 py-4 bg-green-500' onClick={() => add()}>Add</button>
-    <button type="button" className='px-7 py-4 bg-red-500' onClick={() => subtract()}>Subtract</button>
     
     {data.map((item, i) => (
       <div
           id={item.id} 
           key={i} 
-          ref={element => cardRef.current[i] = element} 
-          style={{width: '100%', backgroundColor: `${item.color}`, padding: "10px", marginTop: `${item.position}00px`, position:'absolute'}}
+          style={{width: '100%', backgroundColor: `${item.color}`, padding: "10px", marginTop: `${item.position}00px`, position:'absolute', transition: 'all 0.9s ease-out'}}
       >
         {item.position + " " + item.title}
       </div>
@@ -79,3 +51,4 @@ function List() {
 export default List;
 
 // https://stackoverflow.com/questions/54633690/how-can-i-use-multiple-refs-for-an-array-of-elements-with-hooks
+// https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
